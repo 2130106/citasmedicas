58e853d90f87e2e43acd2800c4813cb5afa121fd
2130106 <2130106@upv.edu.mx>
2024-07-17 08:13:47 -0600

    .

--- resources/views/auth/home.blade.php
+++ resources/views/auth/home.blade.php
@@ -150,6 +150,7 @@
         @if (Auth::user()->role=='admin')
             <a href="{{ route('medicos.index') }}">Médicos</a> 
         @endif
+        <a href="#">Servicios</a>
         @if (Auth::user()->role=='doctor')
             <a href="{{ route('consultas.index') }}">Registro de consultas. </a> 
         @endif
